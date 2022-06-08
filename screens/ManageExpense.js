b5f03dae231b4ee8513a5c639c733a88d3ce1ec6
myntifyX <your_email_id>
2022-06-07 20:16:13 -0400

    Added logic for conditionally rendering the Edit Expense mode or Add Expense mode in the ManageExpense.js modal.

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,56 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  // const editedExpenseId = route.params?.expenseId;
+  // const isEditing = !!editedExpenseId;
+
+  const checkEditing = route.params?.editItem;
+  const isEditing = !!checkEditing;
+  console.log("-----------");
+  console.log(isEditing);
+  // navigation.setOptions({
+  //   title: isEditing ? "Edit Expense" : "Add Expense"
+  // });
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [isEditing]);
+
+  function deleteExpenseHander() {}
+
   return (
-    <Text>Manage Expense Screen</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+        <IconButton 
+        iconName="trash" 
+        color={GlobalStyles.colors.error500} 
+        size={36} 
+        onPress={deleteExpenseHander}/>
+        </View>
+      )}
+    </View>
   );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24, 
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+})
